39f2fe6e78c2f477affd90b50d3230d6caccd589
Marty445 <markovskimarty@gmail.com>
2026-01-15 12:50:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 import streamlit as st
 import pandas as pd
 
-st.title("📊 Любими неща – класна анкета")
+st.title("📊 Оценки и ученици")
 
 # Инициализация на данните
 if "colors" not in st.session_state:
     st.session_state.colors = {
-        "Червен": 0,
-        "Син": 0,
-        "Зелен": 0,
-        "Жълт": 0
+        "Шестица": 0,
+        "Петица": 0,
+        "Четворка": 0,
+        "Тройка": 0
+        "Двойка": 0
     }
 
 if "sports" not in st.session_state:
     st.session_state.sports = {
-        "Футбол": 0,
-        "Баскетбол": 0,
-        "Волейбол": 0,
-        "Плуване": 0
+        "Гощо": 0,
+        "Пешо": 0,
+        "Мария": 0,
+        "Гергана": 0
     }
 
-st.subheader("Избери любими неща")
+st.subheader("Избери ученик и оценка.)
 
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
 sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
